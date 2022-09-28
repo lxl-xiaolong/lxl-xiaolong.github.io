e487d7b7b579a24d991ca5749ed22a21c909f2c2
lxl-xiaolong <113812530+lxl-xiaolong@users.noreply.github.com>
2022-09-28 16:01:05 +0800

    Add files via upload

---
+++ JS/baidufenxi.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?85bf60a6550ebbfae001c334b99a7554";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ JS/nopaste.js
@@ -0,0 +1,15 @@
+document.onpaste = function (event){
+    if(window.event){
+        event = window.event;
+        }
+    try{
+        var the = event.srcElement;
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")){
+        return false;
+        }
+        return true;
+        }
+    catch (e){
+        return false;
+    }
+}
