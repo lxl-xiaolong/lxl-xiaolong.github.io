21657a961c63003eccfcd7977ef5a37ddab3fb25
lxl-xiaolong <113812530+lxl-xiaolong@users.noreply.github.com>
2022-12-26 09:28:04 +0800

    Delete JS directory

--- JS/nof12.js
+++
@@ -1,7 +0,0 @@
-window.onkeydown = window.onkeyup = window.onkeypress = function (event) {
-    // 判断是否按下F12，F12键码为123
-    if (event.keyCode == 123) {
-    event.preventDefault(); // 阻止默认事件行为
-    window.event.returnValue = false;
-    }
-}
