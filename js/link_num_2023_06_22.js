ae074a37f94f3ec6303d8d598bdb7bf93a5954d9
lxl-xiaolong <113812530+lxl-xiaolong@users.noreply.github.com>
2023-11-13 02:32:49 +0800

    Update link_num_2023_06_22.js

--- js/link_num_2023_06_22.js
+++ js/link_num_2023_06_22.js
@@ -1,5 +1,24 @@
 //link number
-			temp = document.getElementById("content");
-			lis = temp.getElementsByTagName("li");
-			var linum = lis.length; //显示li元素的个数
-			LiNum.innerHTML = "本网页各板块已收录链接总计：" + linum + "个。"
+(function() {
+  const CONTENT_ID = "content";
+  const LINUM_ID = "LiNum";
+  const LIS_SELECTOR = "li";
+
+  function updateLiNum() {
+    const contentElement = document.getElementById(CONTENT_ID);
+    const LiNumElement = document.getElementById(LINUM_ID);
+
+    if (contentElement && LiNumElement) {
+      const lis = contentElement.querySelectorAll(LIS_SELECTOR);
+      const linum = lis.length;
+
+      // 使用 textContent 替代 innerHTML
+      LiNumElement.textContent = `本网页各板块已收录链接总计：${linum}个。`;
+    } else {
+      console.error(`未找到必要的元素。请确保存在具有ID为${CONTENT_ID}和${LINUM_ID}的元素。`);
+    }
+  }
+
+  // 调用函数
+  updateLiNum();
+})();
