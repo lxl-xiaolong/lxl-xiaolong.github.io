213cc580d772c213e9aa0f9c8ff3d4a3256bd198
lxl-xiaolong <113812530+lxl-xiaolong@users.noreply.github.com>
2024-08-04 14:13:55 +0800

    Add files via upload

--- tools/python脚本打包工具/setup.py
+++ tools/python脚本打包工具/setup.py
@@ -13,12 +13,12 @@ base = 'Win32GUI' if sys.platform=='win32' else None
 
 options = {'build_exe': build_exe_options}
 
-executables = [Executable("app.py", base=base, icon="app.ico")]
+executables = [Executable("D:/Develop/Python/python脚本打包工具/python脚本打包工具.py", base=base)]
 
 setup(
-    name="",
-    version="",
-    description="",
+    name="python脚本打包工具",
+    version="1.0",
+    description="python脚本打包成exe可执行文件。",
     options=options,
     executables=executables
 )
